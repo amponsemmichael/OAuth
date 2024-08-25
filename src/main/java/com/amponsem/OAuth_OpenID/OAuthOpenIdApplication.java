0c7f3a5127c1772b88286b42c340b493834f99c2
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-25 05:17:18 +0000

    initial project setup

---
+++ src/main/java/com/amponsem/OAuth_OpenID/OAuthOpenIdApplication.java
@@ -0,0 +1,13 @@
+package com.amponsem.OAuth_OpenID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthOpenIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthOpenIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amponsem/OAuth_OpenID/OAuthOpenIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amponsem.OAuth_OpenID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthOpenIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
