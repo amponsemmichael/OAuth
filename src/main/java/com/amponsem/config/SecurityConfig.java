4b49e4bedc524fb2ceb410322a04f7ffcf05685f
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-25 11:19:03 +0000

    add session to prevent 1 user from concurrent session per time

--- src/main/java/com/amponsem/config/SecurityConfig.java
+++ src/main/java/com/amponsem/config/SecurityConfig.java
@@ -1,24 +1,40 @@
-package com.amponsem.config;
 
+package com.amponsem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
-                .authorizeRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults());
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+
+                .csrf(csrfConfigurer -> csrfConfigurer
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+
+                .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .invalidSessionUrl("/login?invalid-session=true")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true))
+
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .deleteCookies("JSESSIONID", "XSRF-TOKEN")
+                        .invalidateHttpSession(true));
 
         return http.build();
     }
 }
+
