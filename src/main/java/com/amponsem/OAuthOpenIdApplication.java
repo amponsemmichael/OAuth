bcde1f5324d6f9e28eb35f079bd5dab44cea827f
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-25 05:40:53 +0000

    modified pom to disable default login page

--- src/main/java/com/amponsem/OAuthOpenIdApplication.java
+++ src/main/java/com/amponsem/OAuthOpenIdApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OAuthOpenIdApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OAuthOpenIdApplication.class, args);
 	}
 
